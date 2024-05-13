5ce05a7dec737e885ed2acc1c97b8427402927f4
Michael Valdes <michael.j.valdes@gmail.com>
2024-05-13 18:53:58 -0400

    Create initial models

--- backend/beanreview/reviews/models.py
+++ backend/beanreview/reviews/models.py
@@ -1,3 +1,23 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Coffee(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    roaster = models.CharField(max_length=100)
+    year = models.IntegerField()
+    origin = models.TextField(max_length=20)
+    # process descriptors
+    # tasting note descriptors
+
+
+class Review(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    rating = models.SmallIntegerField()
+    notes = models.TextField()
+    # tasting note descriptors
+    # owner
+    coffee = models.ForeignKey(
+        Coffee, related_name='coffee', on_delete=models.CASCADE)
