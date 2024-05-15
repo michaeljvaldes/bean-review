1ed856ab76b547a010406f20d8352ada6a57ef4b
Michael Valdes <michael.j.valdes@gmail.com>
2024-05-15 17:41:15 -0400

    Refactory directory structure

--- backend/reviews/models.py
+++ backend/reviews/models.py
@@ -3,10 +3,18 @@ import uuid
 from django.db import models
 
 
+class Roaster(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=50)
+    website = models.CharField(max_length=50)
+    # owner
+
+
 class Coffee(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
-    roaster = models.CharField(max_length=100)
+    roaster = models.ForeignKey(
+        Roaster, related_name='roaster', on_delete=models.CASCADE)
     year = models.IntegerField()
     origin = models.TextField(max_length=20)
     # process descriptors
