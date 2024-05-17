53a478d9ec23b7d67998e918919f985dfda90e2d
Michael Valdes <michael.j.valdes@gmail.com>
2024-05-17 18:22:46 -0400

    Fix reviews grid sizing

--- frontend/src/components/Reviews/Reviews.tsx
+++ frontend/src/components/Reviews/Reviews.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react';
-import { Card, Grid } from '@mui/joy';
+import { Box, Card, Grid } from '@mui/joy';
 import axios from 'axios';
 import { useQuery } from '@tanstack/react-query';
 import Review from '../../models/review';
@@ -23,18 +23,12 @@ const Reviews: FC<ReviewsProps> = () => {
   const query = useReviewsQuery()
 
   return (
-    <Grid data-testid="Reviews" container spacing={2}>
-      {query.data?.flatMap((_, i, a) => i % 2 ? [] : [a.slice(i, i + 2)])
-        .map(reviewPair =>
-        (<>
-          <Grid xs={6}>
-            <ReviewCard review={reviewPair[0]}></ReviewCard>
-          </Grid>
-          <Grid xs={6}>
-            {reviewPair.length > 1 && (<ReviewCard review={reviewPair[1]}></ReviewCard>)}
-          </Grid>
-        </>
-        ))}
+    <Grid data-testid="Reviews" container spacing={2} columns={2}>
+      {query.data?.map(review => (
+        <Grid key={review.id} xs={1}>
+          <ReviewCard review={review}></ReviewCard>
+        </Grid>
+      ))}
     </Grid>
   )
 };
