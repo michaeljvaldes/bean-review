c6c50f64a0e4af1533b722518135885c821d7b47
Michael Valdes <michael.j.valdes@gmail.com>
2024-05-17 02:25:12 -0400

    Add axios and react-query and connect frontend and backend

--- frontend/src/components/Reviews/Reviews.tsx
+++ frontend/src/components/Reviews/Reviews.tsx
@@ -1,12 +1,40 @@
 import React, { FC } from 'react';
+import { Box, Typography } from '@mui/joy';
+import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
+
+type Review = {
+  id: string,
+  name: string,
+  year: number,
+  origin: string,
+  rating: number,
+  notes: string,
+  roaster: string,
+  owner: string
+}
+
+const getReviews = async () => {
+  const response = await axios.get('/reviews.json')
+  const data: Review[] = response.data
+  return data
+}
+
+const useReviewsQuery = () => {
+  return useQuery({ queryKey: ['reviews'], queryFn: getReviews })
+}
 
 
 interface ReviewsProps { }
 
-const Reviews: FC<ReviewsProps> = () => (
-  <div data-testid="Reviews">
-    Reviews Component
-  </div>
-);
+const Reviews: FC<ReviewsProps> = () => {
+  const query = useReviewsQuery()
+  return (
+    <Box data-testid="Reviews">
+      {query.data?.map(review =>
+        <Typography>{review.name}</Typography>)}
+    </Box>
+  )
+};
 
 export default Reviews;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,8 @@ import { CssVarsProvider } from '@mui/joy'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Reviews from './components/Reviews/Reviews.tsx'
 import Roasters from './components/Roasters/Roasters.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import axios from 'axios'
 
 const router = createBrowserRouter([
   {
@@ -23,10 +25,15 @@ const router = createBrowserRouter([
   }
 ])
 
+axios.defaults.baseURL = 'http://localhost:8000/'
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <CssVarsProvider>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </CssVarsProvider>
   </React.StrictMode>,
 )
