a094d124989cb366b33af62b832365941c2eaff9
Michael Valdes <michael.j.valdes@gmail.com>
2024-05-19 14:40:13 -0400

    Add infinite scrolling for Reviews

--- frontend/src/components/Reviews/Reviews.tsx
+++ frontend/src/components/Reviews/Reviews.tsx
@@ -1,39 +1,50 @@
-import React, { FC, useState } from 'react';
+import React, { FC } from 'react';
 import { Grid } from '@mui/joy';
 import axios from 'axios';
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
 import Review from '../../models/review';
 import ReviewCard from '../ReviewCard/ReviewCard';
 import PaginatedResonse from '../../models/paginatedResponse';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 
-const PAGE_SIZE = 100
-
-const getReviews = async (limit = PAGE_SIZE) => {
-  const response = await axios.get(`/reviews.json/?limit=${limit}`)
+const getReviews = async ({ pageParam }: { pageParam: string }) => {
+  const response = await axios.get(`/reviews.json/?page=${pageParam}`)
   const data: PaginatedResonse<Review> = response.data
   return data
 }
 
-const useReviewsQuery = (limit: number) => {
-  return useQuery({ queryKey: ['reviews', limit], queryFn: () => getReviews(limit) })
+const useReviewsQuery = () => {
+  return useInfiniteQuery({
+    queryKey: ['reviews'],
+    queryFn: getReviews,
+    initialPageParam: '0',
+    getNextPageParam: (lastPage) => lastPage.next
+  })
 }
 
 interface ReviewsProps { }
 
 const Reviews: FC<ReviewsProps> = () => {
-  const [limit, setLimit] = useState(PAGE_SIZE)
-  const query = useReviewsQuery(limit)
+  const { data, fetchNextPage, hasNextPage } = useReviewsQuery()
 
   return (
-    <Grid data-testid="Reviews" container spacing={2} columns={2}>
-      {query.data?.results.map(review => (
-        <Grid key={review.id} xs={1} display={'flex'} flexDirection={'column'}>
-          <ReviewCard review={review}></ReviewCard>
-        </Grid>
-      ))
-      }
-    </Grid >
+    <InfiniteScroll
+      dataLength={data?.pages.length ?? 0}
+      next={fetchNextPage}
+      hasMore={hasNextPage}
+      loader={<h4>Loading...</h4>}
+    >
+      <Grid data-testid="Reviews" container spacing={2} columns={2}>
+        {data?.pages.flatMap(page => page.results)
+          .map(review => (
+            <Grid key={review.id} xs={1} display={'flex'} flexDirection={'column'}>
+              <ReviewCard review={review}></ReviewCard>
+            </Grid>
+          ))
+        }
+      </Grid >
+    </InfiniteScroll>
   )
 };
 
